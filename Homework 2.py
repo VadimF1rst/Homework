9fb15b7498581024b936eb076141f81921c18085
VadimF1rst <90533950+VadimF1rst@users.noreply.github.com>
2021-09-22 13:30:41 +0300

    Add files via upload

---
+++ Homework 2.py
@@ -0,0 +1,26 @@
+# BEGIN
+print(8 / 2 + 5 - -3 / 2)
+# END
+# BEGIN
+print("Khal Drogo\'s favorite word is \"athjahakar\"")
+# END
+# BEGIN
+a = "Winter came"
+b="for the House of Frey."
+print (a + " " + b)
+# END
+# BEGIN
+word = "First text"
+print (word)
+'First text'
+word = "Second text"
+print (word)
+'Second text'
+# END
+# BEGIN
+print("What's your name?")
+a = input()
+print("Hello " + a + "!")
+# END
+
+
